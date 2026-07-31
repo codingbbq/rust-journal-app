e85594ac1288f097db4aad9a898c0e2941e04b60
codingbbq <codingbbq@gmail.com>
2026-08-01 01:13:18 +0530

    Adding data to the .csv

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,71 @@
-use std::fs::OpenOptions;
-use std::io;
+use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write};
 use std::path::Path;
 
 fn main() {
-    match ensure_journal_file() {
-        Ok(()) => println!("Journey.csv is ready."),
-        Err(err) => eprintln!("Failed to prepare Journey.csv : {}", err),
+    let args: Vec<String> = env::args().collect();
+
+    match run(args) {
+        Ok(()) => {}
+        Err(err) => eprintln!("Error : {}", err),
     }
 }
 
-fn ensure_journal_file() -> io::Result<()> {
-    let file_path = Path::new("Journey.csv");
+fn run(args: Vec<String>) -> io::Result<()> {
+    ensure_journal_file()?;
 
-    match OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(file_path)
-        {
-            Ok(_) => {
-                println!("Journey.csv created");
-                Ok(())
-            }
-            Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
-                println!("Joureny.csv already exists");
-                Ok(())
-            }
+    if args.len() < 2 {
+        print_help();
+        return Ok(());
+    }
 
-            Err(err) => Err(err),
+    let command = &args[1];
+
+    if command == "add" {
+        if args.len() < 3 {
+            eprintln!("Please provide journal text");
+            eprintln!("Example: cargo run -- add \"Today I learned Rust basics\"");
+            return Ok(());
         }
+
+        let entry_text = args[2..].join(" ");
+        append_entry(&entry_text)?;
+        println!("Entry saved");
+    } else {
+        eprintln!("Unknown command: {}", command);
+        print_help();
+    }
+
+    Ok(())
+}
+
+fn ensure_journal_file() -> io::Result<()> {
+    let file_path = Path::new("Journal.csv");
+
+    if !file_path.exists() {
+        let mut file = File::create(file_path)?;
+        file.write_all(b"entry\n")?;
+        println!("Journey.csv created with header");
+    }
+
+    Ok(())
+}
+
+fn append_entry(entry_text: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+    .append(true)
+    .open("Journey.csv")?;
+
+    writeln!(file, "\"{}\"", escape_csv(entry_text))?;
+    Ok(())
+}
+
+fn escape_csv(input: &str) -> String {
+    input.replace('\"', "\"\"")
+}
+
+fn print_help() {
+    println!("Usage:");
+    println!(" Cargo run -- add \"your journey entry\"");
 }
